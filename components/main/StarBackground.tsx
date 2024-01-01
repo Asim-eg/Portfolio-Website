f58bb380bad546242bbb60fc32073fb930fda6ce
Asim Altaf <asim.altaf@zixel.cn>
2024-01-01 18:57:20 +0500

    【Develop】 Added Navbar Component

--- components/main/StarBackground.tsx
+++ components/main/StarBackground.tsx
@@ -44,7 +44,7 @@ const StarsCanvas = () => (
   <div className='w-full h-auto fixed inset-0 z-[20]'>
     <Canvas camera={{position:[0,0,1]}}>
       <Suspense fallback={null}>
-        <Preload all />
+      <Preload all />
         <StarBackground />
       </Suspense>
     </Canvas>
