e261475f138f748c471f4c5b94cbf43b84dfcdbf
Asim Altaf <asim.altaf@zixel.cn>
2024-01-02 11:22:19 +0500

    【Update】 Added Hover Animation in Navbar

--- components/main/Navbar.tsx
+++ components/main/Navbar.tsx
@@ -1,8 +1,11 @@
 import { Socials } from '@/constants'
 import Image from 'next/image'
 import React from 'react'
+import { workerData } from 'worker_threads';
 
 const Navbar = () => {
+
+  
   return (
     <div
     className='w-full h-[65px] fixed top-0 shadow-lg shadow-[#2A0E61]/50 bg-[#03001417] backdrop-blur-md z-50 px-10'
@@ -27,12 +30,12 @@ const Navbar = () => {
         </a>
 
         <div className='w-[500px] h-full flex flex-row items-center justify-between md:mr-20'>
-          <div className='w-full h-auto flex items-center justify-between border border-[#7042f861] bg-[#0300145e] mr-[15px] px-[20px] py-[10px] rounded-full'>
-            <a className='cursor-pointer text-gray-300' href='#about-me'>About me</a>
-            <a className='cursor-pointer text-gray-300' href='#skills'>Skills</a>
-            <a className='cursor-pointer text-gray-300' href='#projects'>Projects</a>
-          </div>
-        </div>
+      <div className='w-full h-auto flex items-center justify-between border border-[#7042f861] bg-[#0300145e] mr-[15px] px-[20px] py-[10px] rounded-full'>
+        <a className='nav-link' href='#about-me'>About Me</a>
+        <a className='nav-link' href='#skills'>Skills</a>
+        <a className='nav-link' href='#projects'>Projects</a>
+      </div>
+    </div>
 
         <div
         className='flex flex-row gap-5'
