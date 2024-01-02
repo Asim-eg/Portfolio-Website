ab720bdc60ebcc321d107ee676fb3512c00484b6
Asim Altaf <asim.altaf@zixel.cn>
2024-01-02 13:09:47 +0500

    【Update】 Added Encryption and Fixed Layering

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,9 +19,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${inter.className} bg-[#030014] overflow-y-scroll overflow-x-hidden`}>
-        {children}
         <Navbar />
         <StarsCanvas />
+        {children}
       </body>
     </html>
   )
