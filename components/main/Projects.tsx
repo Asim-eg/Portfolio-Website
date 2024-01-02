6f22bab2a3430a53168dcc8c3e92a7dccc677997
Asim Altaf <asim.altaf@zixel.cn>
2024-01-02 13:24:38 +0500

    【Update】 Added Projects Section

---
+++ components/main/Projects.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import ProjectCard from '../sub/ProjectCard';
+
+const Projects = () => {
+  return (
+    <div className="flex flex-col items-center justify-center py-20">
+      <h1 className="text-[40px] font-semibold text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-cyan-500 py-20">
+        My Projects
+      </h1>
+      <div className="h-full w-full flex flex-col md:flex-row gap-10 px-10">
+        <ProjectCard
+          src="/NextWebsite.png"
+          title="Modern Next.js Website"
+          description="A modern Next.js website with a dark mode toggle and a contact form."
+        />
+
+        <ProjectCard
+          src="/CardImage.png"
+          title="Interactive Website Cards"
+          description="Interactive website cards with a 3D tilt effect and a custom cursor."
+        />
+
+        <ProjectCard
+          src="/SpaceWebsite.png"
+          title="A Portfolio Website"
+          description="A portfolio website with a space theme and a contact form."
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Projects;
